ee9935642a3ba51b664535ccb6f184b6279e16c1
Chae-Yeun Park <chae-yeun@yonsei.ac.kr>
2026-03-30 11:26:22 +0900

    Add all

---
+++ week04/simd-benchmark/include/MyMatrix.hpp
@@ -0,0 +1,74 @@
+#include "MyVector.hpp"
+
+#include <cstddef>
+#include <random>
+
+enum class Order {RowMajor, ColMajor};
+
+template<Order order_>
+struct MyMatrix {
+    constexpr static Order order = order_;
+    size_t rows_;
+    size_t cols_;
+    double* data_;
+
+    MyMatrix(size_t rows, size_t cols) {
+        data_ = new double[rows*cols];
+    }
+
+    inline double& operator()(size_t i, size_t j) {
+        if constexpr (order == Order::RowMajor) {
+            return data_[i*cols_ + j];
+        } else {
+            return data_[j*rows_ + i];
+        }
+    }
+
+    inline const double& operator()(size_t i, size_t j) const {
+        if constexpr (order == Order::RowMajor) {
+            return data_[i*cols_ + j];
+        } else {
+            return data_[j*rows_ + i];
+        }
+    }
+
+    size_t rows() const {
+        return rows_;
+    }
+
+    size_t cols() const {
+        return cols_;
+    }
+
+    ~MyMatrix() {
+        delete[] data_;
+    }
+};
+
+template<Order order, typename RandomEngine>
+MyMatrix<order> random_matrix(RandomEngine& re, size_t rows, size_t cols) {
+    MyMatrix<order> mat(rows, cols);
+    std::normal_distribution ndist;
+    for(size_t i = 0; i < rows; i++) {
+        for(size_t j = 0; j < cols; j++) {
+            mat(i, j) = ndist(re);
+        }
+    }
+    return mat;
+}
+
+template<Order order>
+MyVectorDouble matrix_vector_prod(const MyMatrix<order>& mat, const MyVectorDouble& vec) {
+    assert(mat.cols() == vec.size());
+
+    MyVectorDouble res(mat.rows());
+    for(size_t i = 0; i < mat.rows(); i++) {
+        double val = 0.0;
+        for(size_t j = 0; j < mat.cols(); j++) {
+            val += mat(i,j) * vec[j];
+        }
+        res[i] = val;
+    }
+
+    return res;
+}
--- week04/simd-benchmark/src/main.cpp
+++ week04/simd-benchmark/src/main.cpp
@@ -1,5 +1,5 @@
 #include "MyVector.hpp"
-
+#include "MyMatrix.hpp"
 #include <benchmark/benchmark.h>
 
 #include <list>
@@ -47,6 +47,22 @@ static void BM_dot_fma_unroll(benchmark::State& state) {
 BENCHMARK(BM_dot_fma_unroll)->Arg(1U << 10)->Arg(1U << 12)->Arg(1U << 14)->Arg(1U << 16);
 BENCHMARK(BM_dot_fma)->Arg(1U << 10)->Arg(1U << 12)->Arg(1U << 14)->Arg(1U << 16);
 BENCHMARK(BM_dot)->Arg(1U << 10)->Arg(1U << 12)->Arg(1U << 14)->Arg(1U << 16);
+
+template<Order order>
+static void BM_matrix_vector_prod(benchmark::State& state) {
+	std::default_random_engine re{1557};
+	for (auto _ : state) {
+		state.PauseTiming();
+		auto mat = random_matrix<order>(re, state.range(0), state.range(0));
+		auto vec = random_vector(re, state.range(0));
+		state.ResumeTiming();
+		auto res = matrix_vector_prod(mat, vec);
+		benchmark::DoNotOptimize(res);
+	}
+}
+
+BENCHMARK(BM_matrix_vector_prod<Order::RowMajor>)->Arg(1U << 5);
+BENCHMARK(BM_matrix_vector_prod<Order::ColMajor>)->Arg(1U << 5);
 
 BENCHMARK_MAIN();
 
